0071df1d7923a85f5b71e89610d3e827802fd65d
Dennis <dennis.effa.amponsah@accenture.com>
2025-08-05 10:17:15 +0100

    update test method

--- src/common/helpers/mongodb.test.js
+++ src/common/helpers/mongodb.test.js
@@ -45,7 +45,7 @@ describe('#mongoDb', () => {
       const closeSpy = vi.spyOn(server.mongoClient, 'close')
       await server.stop({ timeout: 0 })
 
-      expect(closeSpy).toHaveBeenCalled(true)
+      expect(closeSpy).toHaveBeenCalledWith(true)
     })
   })
 })
