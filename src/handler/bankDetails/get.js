cbfc266fd0de2925133885df7c53f5490e186091
juhiupreti-acc <juhi.upreti@accenture.com>
2025-09-11 10:54:04 +0100

    Return masked bank details the role os other than CEO

--- src/handler/bankDetails/get.js
+++ src/handler/bankDetails/get.js
@@ -1,25 +1,37 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 const getBankDetails = async (request, h) => {
-    try {
-      // Retrieve localAuthority (and optionally role) from JWT
-      const { localAuthority, role } = request.auth.credentials
-  
-      // Call the mock API and pass localAuthority as query param
-      const response = await fetch(
-        `https://laps-api-mock-bank-details.dev.cdp-int.defra.cloud/bank-details?localAuthority=${encodeURIComponent(localAuthority)}`
-      )
-  
-      if (!response.ok) {
-        throw new Error(`External API error: ${response.status}`)
-      }
-  
-      const data = await response.json()
-  
-      return h.response(data).code(200)
-    } catch (err) {
-      console.error('Error fetching bank details:', err)
-      return h.response({ error: 'Failed to fetch bank details' }).code(500)
+  try {
+    // Retrieve localAuthority (and optionally role) from JWT
+    const { localAuthority, role } = request.auth.credentials
+
+    // Call the mock API and pass localAuthority as query param
+    const BASE_URL = process.env.API_URL
+    const response = await fetch(
+      `${BASE_URL}/bank-details?localAuthority=${encodeURIComponent(localAuthority)}`
+    )
+
+    if (!response.ok) {
+      throw new Error(`External API error: ${response.status}`)
     }
+
+    let bankDetails = await response.json()
+
+    if (role?.toUpperCase() !== 'CEO' && bankDetails?.sortcode) {
+      const lastTwoDigits = bankDetails.sortcode.slice(-2)
+      const masked = 'ending with' + lastTwoDigits
+      bankDetails = {
+        ...bankDetails,
+        sortcode: masked
+      }
+    }
+
+    return h.response(bankDetails).code(200)
+  } catch (err) {
+    console.error('Error fetching bank details:', err)
+    return h.response({ error: 'Failed to fetch bank details' }).code(500)
   }
-  
-  export { getBankDetails }
-  
+}
+
+export { getBankDetails }
