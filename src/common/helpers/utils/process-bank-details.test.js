209ef17808dbc324a748ad47e41b9938028d577a
juhiupreti-acc <juhi.upreti@accenture.com>
2025-09-29 11:01:21 +0100

    Add tests for HOF and non HOF

--- src/common/helpers/utils/process-bank-details.test.js
+++ src/common/helpers/utils/process-bank-details.test.js
@@ -15,4 +15,18 @@ describe('processBankDetails', () => {
     const output = processBankDetails(input, role)
     expect(output.sortCode).toBe('ending with 56')
   })
+
+  it('returns unmasked sortcode for Head of Finance role', () => {
+    const input = { sortCode: '987654', confirmed: true }
+    const role = 'Head of Finance'
+    const output = processBankDetails(input, role)
+    expect(output.sortCode).toBe('987654')
+  })
+
+  it('masks sortcode for non-CEO and non-HOF roles', () => {
+    const input = { sortCode: '123456', confirmed: true }
+    const role = 'Staff'
+    const output = processBankDetails(input, role)
+    expect(output.sortCode).toBe('ending with 56')
+  })
 })
