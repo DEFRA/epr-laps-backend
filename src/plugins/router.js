09284bc1abafcbe731700631796666d3524c0f3d
juhiupreti-acc <juhi.upreti@accenture.com>
2025-10-08 10:58:54 +0100

    Fix review comment

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -3,7 +3,7 @@ import { example } from '../routes/example.js'
 import { bankDetailsRoutes } from '../routes/bankDetails.js'
 import { fileRoutes } from '../routes/documents.js'
 
-export const router = {
+const router = {
   plugin: {
     name: 'router',
     register: (server, _options) => {
@@ -11,3 +11,5 @@ export const router = {
     }
   }
 }
+
+export { router }
