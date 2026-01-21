2d84aa6952bf8ec5ecb6025b6de185d13336cf86
juhiupreti-acc <juhi.upreti@accenture.com>
2026-01-21 12:42:21 +0000

    Remove example files

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,5 +1,4 @@
 import { health } from '../routes/health.js'
-import { example } from '../routes/example.js'
 import { bankDetailsRoutes } from '../routes/bankDetails.js'
 import { fileRoutes } from '../routes/documents.js'
 import { accessControl } from '../routes/access-control.js'
@@ -9,7 +8,6 @@ const router = {
     name: 'router',
     register: (server, _options) => {
       const allRoutes = [health]
-        .concat(example)
         .concat(bankDetailsRoutes)
         .concat(fileRoutes)
         .concat(accessControl)
