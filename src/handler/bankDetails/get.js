a988d08853dc031774d733ef45dec3926174301d
juhiupreti-acc <juhi.upreti@accenture.com>
2026-02-12 12:16:58 +0000

    Setting statuscodes in audit logs

--- src/handler/bankDetails/get.js
+++ src/handler/bankDetails/get.js
@@ -51,7 +51,8 @@ const getBankDetails = async (request, h) => {
       writeBankDetailsAuditLog(
         request.auth.isAuthorized,
         request,
-        Outcome.Failure
+        Outcome.Failure,
+        response.status
       )
       throw Boom.internal(`Failed to fetch bank details`)
     }
@@ -79,15 +80,20 @@ const getBankDetails = async (request, h) => {
     writeBankDetailsAuditLog(
       request.auth.isAuthorized,
       request,
-      Outcome.Success
+      Outcome.Success,
+      response.ok
     )
     return h.response(processedDetails).code(statusCodes.ok)
   } catch (err) {
-    request.logger.error(`Error fetching bank details: ${JSON.stringify(err)}`)
+    const statusCode = err.output?.statusCode || statusCodes.internalServerError
+    request.logger.error(
+      `Error fetching bank details for logger: ${JSON.stringify(err)}`
+    )
     writeBankDetailsAuditLog(
       request.auth.isAuthorized,
       request,
-      Outcome.Failure
+      Outcome.Failure,
+      statusCode
     )
     throw Boom.internal('Failed to fetch bank details')
   }
@@ -98,11 +104,22 @@ export { getBankDetails }
 export const writeBankDetailsAuditLog = (
   canViewFullBankDetails,
   request,
-  outcome
+  outcome,
+  statusCode
 ) => {
   if (canViewFullBankDetails) {
-    writeAuditLog(request, ActionKind.FullBankDetailsViewed, outcome)
+    writeAuditLog(
+      request,
+      ActionKind.FullBankDetailsViewed,
+      outcome,
+      statusCode
+    )
     return
   }
-  writeAuditLog(request, ActionKind.MaskedBankDetailsViewed, outcome)
+  writeAuditLog(
+    request,
+    ActionKind.MaskedBankDetailsViewed,
+    outcome,
+    statusCode
+  )
 }
