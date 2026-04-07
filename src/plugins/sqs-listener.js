f468bae232ed495a48f31c5a377dba30004d7a96
Dennis <dennis.effa.amponsah@accenture.com>
2026-04-07 08:42:22 +0100

    updates queue name

--- src/plugins/sqs-listener.js
+++ src/plugins/sqs-listener.js
@@ -70,7 +70,7 @@ const sqsListener = {
 const costDataFormListener = {
   plugin: sqsListener,
   options: {
-    queueName: 'demo-queue.fifo',
+    queueName: 'epr-laps-costdata-form.fifo',
     onmessage: async (server, message) => {
       // Process the message here
       server.logger.info(`Received message for cost data form: ${message.Body}`)
